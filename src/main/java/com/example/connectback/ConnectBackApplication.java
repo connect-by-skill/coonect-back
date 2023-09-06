2bac813d2507a79941245d5b2bde94088cde22a9
Jeonwoojae <dnwo0719@icloud.com>
2023-09-06 16:45:27 +0900

    init project

---
+++ src/main/java/com/example/connectback/ConnectBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.connectback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/connectback/ConnectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.connectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
